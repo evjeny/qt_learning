6062ecebbfd46b2525c52044bdd12d76e6b8a75f
evjeny <gkanafing@gmail.com>
2021-07-11 18:23:00 +0500

    add widgets example

---
+++ lessons/1/a.py
@@ -0,0 +1,36 @@
+import sys
+from PySide2.QtGui import QPixmap
+from PySide2.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        label = QLabel("I am label")
+        layout.addWidget(label)
+
+        button = QPushButton(text="I am button")
+        layout.addWidget(button)
+
+        image = QLabel()
+        pixmap = QPixmap()
+        with open("assets/python_logo.png", "rb") as f:
+            content = f.read()
+        pixmap.loadFromData(content)
+        image.setPixmap(pixmap)
+        layout.addWidget(image)
+
+        self.setLayout(layout)
+        self.setWindowTitle('Widgets')
+        self.showMaximized()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    ex = Example()
+    sys.exit(app.exec_())
